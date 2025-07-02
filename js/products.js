d8eb00d3cb1f3075b12c33f4d264e28c667c032a
dao <daolq.liin@gmail.com>
2025-07-02 14:45:48 +0700

    update home

---
+++ js/products.js
@@ -0,0 +1,17 @@
+// js/products.js
+export const products = [
+  {
+    name: "HUSKIN WHITE BODY TONE UP SERUM",
+    description: "Giúp dưỡng da mềm mịn, nâng tone, dưỡng ẩm cho da, chống nắng và làm trắng sáng cho da",
+    price: "450.000đ",
+    image: "img/fruite-item-5.jpg",
+    link: "/product/sp001.html"
+  },
+  {
+    name: "HUSKIN WHITE BODY TONE UP SERUM",
+    description: "Giúp dưỡng da mềm mịn, nâng tone, dưỡng ẩm cho da, chống nắng và làm trắng sáng cho da",
+    price: "450.000đ",
+    image: "img/fruite-item-5.jpg",
+    link: "/product/sp001.html"
+  }
+];
