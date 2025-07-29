f56e03ce62d0536d65a57d243b27e9f60f4bdc09
Lê Quang Đạo <qdao98nd@gmail.com>
2025-07-29 17:04:49 +0700

    fix home

--- js/products.js
+++ js/products.js
@@ -1,17 +1,25 @@
 // js/products.js
 export const products = [
   {
-    name: "HUSKIN WHITE BODY TONE UP SERUM",
-    description: "Giúp dưỡng da mềm mịn, nâng tone, dưỡng ẩm cho da, chống nắng và làm trắng sáng cho da",
+    name: "MP0350 - Gel rửa mặt Oxy tươi Chuskin",
+    description: "Làm sạch da, giữ ẩm cho da giúp da mềm mịn",
     price: "450.000đ",
-    image: "img/fruite-item-5.jpg",
-    link: "/product/sp001.html"
+    image: "/assets/images/MP0350.jpg",
+    link: "/product/MP0350/"
   },
   {
-    name: "HUSKIN WHITE BODY TONE UP SERUM",
+    name: "MP1061 - Kem truyền trắng Body Chuskin",
     description: "Giúp dưỡng da mềm mịn, nâng tone, dưỡng ẩm cho da, chống nắng và làm trắng sáng cho da",
     price: "450.000đ",
-    image: "img/fruite-item-5.jpg",
-    link: "/product/sp001.html"
+    image: "/assets/images/MP1061.jpg",
+    link: "/product/MP1061"
+  },
+  {
+    name: "MP0750 - Kem chống nắng Chuskin",
+    description: "Kem chống nắng kiềm dầu, kháng viêm giảm mụn, giảm thâm nám, mờ tàn nhang",
+    price: "450.000đ",
+    image: "/assets/images/MP0750.jpg",
+    link: "/product/MP0750"
   }
+
 ];
